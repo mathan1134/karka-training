6e5e63c5d15b8729525c3ddaa1a300ae32a6d05d
mathan1134 <150134846+mathan1134@users.noreply.github.com>
2023-12-11 21:46:38 +0530

    Delete 1Main class directory

--- 1Main class/DAY11_18_11_2023/practice/index.js
+++
@@ -1,9 +0,0 @@
-let selectbox=document.getElementById("selectbox")
-selectbox.addEventListener("change",handlechange)
-function handlechange(){
-            let selectbox=document.getElementById("selectbox").value
-            console.log(selectbox)
-            let para =document.getElementById("para")
-            para.innerHTML=selectbox
-        }
-
