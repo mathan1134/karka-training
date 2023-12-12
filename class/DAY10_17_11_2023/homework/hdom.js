8e49959190c46e315a43dcdcedcc155bcd9823a6
mathan1134 <150134846+mathan1134@users.noreply.github.com>
2023-12-12 21:38:08 +0530

    Delete class directory (#1)
    class

--- class/DAY10_17_11_2023/homework/hdom.js
+++
@@ -1,7 +0,0 @@
-function inputs(fg){
-    fg.preventDefault()
-    var input1=document.getElementById("input1").value
-    var input2=document.getElementById("input2").value
-console.log(input1)
-    console.log(input2)
-    }
